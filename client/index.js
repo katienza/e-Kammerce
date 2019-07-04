25fe478dc643660f579cc2f862049bb64fe222ef
Ken Atienza <atienza.ken@gmail.com>
2019-07-03 18:52:02 -0700

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import '../public/style.css';
+import App from './app';
+
+const app = (
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
