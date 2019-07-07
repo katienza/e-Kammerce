f682c903dd713937b6062451f85ee7ec4fd60e57
Ken Atienza <atienza.ken@gmail.com>
2019-07-06 23:28:22 -0700

    Added onSubmit and onChange functionality to form.

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class UserHome extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+  }
+
+  async handleSubmit() {
+    try {
+      const user = this.state.username
+      axios.post('http://localhost:3000/api/', {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  handleUsernameChange(event) {
+    event.preventDefault()
+    this.setState({
+      username: event.target.value,
+    });
   }
 
   render() {
     return (
       <div className='login'>
-        <form className='login-form'>
+        <form
+          className='login-form'
+          onSubmit={this.handleSubmit}>
           <label className='login-label'>
             Please Log in To enter the site
           </label>
           <p className='login-input'>
-            <span className='login-txt'>Enter your name:</span>
+            <span className='login-txt'>
+              Enter your name:
+            </span>
             <input
               className='name-input'
               type='text'
-              maxlength='255'
+              value={this.state.username}
+              maxLength='255'
+              onChange={this.handleUsernameChange}
               required
             />
           </p>
